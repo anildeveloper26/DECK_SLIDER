a1f5144de71d895bba4aba3bbd6595e5126a157a
ANIL <anilkumarbobba26@gmail.com>
2025-01-27 10:56:08 +0530

    slider

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        screens: {
+          xs: "5px",
+          xl: "1280px",
+          "2xl": "1220px",
+          "3xl": "2560px",
+          "4xl": "3840px",
+        },
+      },
+    },
+  };
