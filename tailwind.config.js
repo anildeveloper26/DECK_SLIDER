85d6536bff6a004093edec0e8067cb8795eb7560
ANIL <anilkumarbobba26@gmail.com>
2025-01-29 13:02:00 +0530

    screen sizes has been fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        screens: {
-          xs: "5px",
-          xl: "1280px",
-          "2xl": "1220px",
-          "3xl": "2560px",
-          "4xl": "3840px",
-        },
+  theme: {
+    extend: {
+      screens: {
+         "xl": "1152px",
+        '2xl': "1280px",
+        "2.5xl":"1300px",
+        "3xl": "2560px",
+        "4xl": "3840px",
       },
     },
-  };
+  },
+};
