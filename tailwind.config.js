da978d500a58223d6af816681a225a8baa395ac1
ANIL <anilkumarbobba26@gmail.com>
2025-02-05 12:59:29 +0530

    Deck 20 pages are completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ tailwind.config = {
     extend: {
       screens: {
          "xl": "1052px",
-        '2xl': "1280px",
+        '2xl': "1300px",
         "2.5xl":"1400px",
         "3xl": "2560px",
         "4xl": "3840px",
